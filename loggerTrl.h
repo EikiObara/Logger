5eebcb6ff398e47985844dddaffd5559c8f72aae
Sasaki1101 <txy74924@st.yamagata-u.ac.jp>
2018-04-19 19:57:36 +0900

    ログ書き出しディレクトリの自動生成機能を追加。

--- loggerTrl.h
+++ loggerTrl.h
@@ -1,3 +1,6 @@
+#ifndef __LOGGER_TRL_H__
+#define __LOGGER_TRL_H__
+
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <iostream>
@@ -5,96 +8,108 @@
 #include <fstream>
 #include <string>
 #include <ctime>
-#include "Eigen/Core"
+
+#include <sys/stat.h>
+#include <sys/types.h>
+
+//#include "Eigen/Core"
 
 namespace Trl{
 
-std::string TimeStamp(void){
-	std::time_t result = std::time(nullptr);
-	std::ostringstream timeName;
-
-#ifdef __linux__
-	std::tm *pnow = std::localtime(&result);
-	timeName << pnow->tm_year + 1900 << pnow->tm_mon + 1 << pnow->tm_mday
-		<< pnow->tm_hour << pnow->tm_min << pnow->tm_sec;
-#elif _WIN32
-	std::tm pnow;
-	localtime_s(&pnow,&result);
-	timeName << pnow.tm_year + 1900 << pnow.tm_mon + 1 << pnow.tm_mday
-		<< pnow.tm_hour << pnow.tm_min << pnow.tm_sec;
-#else
-	std::tm *pnow = std::localtime(&result);
-#endif
-	
-
-	return std::string(timeName.str());
-}
 
 class Logger{
 private:
 	std::string fileName;
 	std::ostringstream stack;
 public:
-	Logger();
-	~Logger();
-	Logger(const std::string name);
-
-	void Write();	//ex.) call in destractor... (please run when the terminate job)
-
-	void SetFileName(const std::string name);
-	void SetData(Eigen::MatrixXd data);
-	template <typename T> void SetData(const T data, const int num);
-	template <typename T> void SetSentence(const T data);
-};
-
-Logger::Logger(){
-	stack.str("");
-}
-
-Logger::~Logger(){
-}
-
-void Logger::Write(){
-	if(fileName.size() < 1){
-		std::cout << "*** file name no setting ***" << std::endl;
-		std::cout << "*** cannot output data   ***" << std::endl;
+	Logger(){
+		stack.str("");
 	}
 
-	fileName += "_" + TimeStamp() + ".txt";
+	Logger(const std::string name){
+		fileName = name;
+	}
 
-	std::ofstream ofs(fileName.c_str(),std::ios::out);
+	std::string TimeStamp(void){
+		std::time_t result = std::time(nullptr);
+		std::ostringstream timeName;
+	
+	#ifdef __linux__
+		std::tm *pnow = std::localtime(&result);
+		timeName << pnow->tm_year + 1900 << pnow->tm_mon + 1 << pnow->tm_mday
+			<< pnow->tm_hour << pnow->tm_min << pnow->tm_sec;
+	#elif _WIN32
+		std::tm pnow;
+		localtime_s(&pnow,&result);
+		timeName << pnow.tm_year + 1900 << pnow.tm_mon + 1 << pnow.tm_mday
+			<< pnow.tm_hour << pnow.tm_min << pnow.tm_sec;
+	#else
+		std::tm *pnow = std::localtime(&result);
+	#endif
+		
+	
+		return std::string(timeName.str());
+	}
 
-	ofs << stack.str() << std::endl;
+	//ex.) call in destractor... (please run when the terminate job)
+	void Write(){
+		if(fileName.size() < 1){
+			std::cout << "*** file name no setting ***" << std::endl;
+			std::cout << "*** cannot output data   ***" << std::endl;
+		}
 
-}
+		std::string file = "./log";
+		struct stat st;			//ディレクトリ存在確認用構造体
 
-Logger::Logger(const std::string name){
-	fileName = name;
-}
+		if(stat(file.c_str(), &st) != 0){
+				mkdir(file.c_str(),0775);
+				return;
+		}
 
-void Logger::SetFileName(const std::string name){
-	fileName = name;
-}
+		std::string path;
+		
+		path = "./log/" + TimeStamp() + "_" + fileName + ".txt";
 
-void Logger::SetData(Eigen::MatrixXd data){
-	for(int i = 0; i < data.rows();++i){
-		for(int j = 0; j < data.cols(); ++j){
-			stack << data(i,j) << "\t";
+		std::ofstream ofs(path,std::ios::out);
+		ofs << stack.str() << std::endl;
+	}
+
+	void SetFileName(const std::string name){
+		fileName = name;	
+	}
+	//void SetData(Eigen::MatrixXd data);
+	template <typename T> void SetData(const T data, const int num){
+		for(int i = 0; i < num; ++i){
+   			stack << data[i];
+   			stack << "\t";
+   		}
+		
+		stack << std::endl;
+	}
+
+	template <typename T> void SetData(const std::vector<T> data){
+		for(int i = 0, n = data.size(); i < n; ++i){
+			stack << data[i] << "\t";
 		}
 		stack << std::endl;
 	}
-}
 
-template <typename T> void Logger::SetSentence(const T data){
-	stack << data << std::endl;
-}
-
-template <typename T> void Logger::SetData(const T data, const int num){
-	for(int i = 0; i < num; ++i){
-		stack << data[i];
-		stack << "\t";
+	template <typename T> void SetSentence(const T data){
+		stack << data << std::endl;
 	}
-	stack << std::endl;
-}
+};
+
+//void Logger::SetData(Eigen::MatrixXd data){
+//	for(int i = 0; i < data.rows();++i){
+//		for(int j = 0; j < data.cols(); ++j){
+//			stack << data(i,j) << "\t";
+//		}
+//		stack << std::endl;
+//	}
+//}
+
+
 
 }	//namespace Trl
+
+#endif //__LOGGER_TRL_H__
